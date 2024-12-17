347e6c0123a2c5e98553a5152613962c330e5248
SamAlrahhal <95586168+SamAlrahhal@users.noreply.github.com>
2024-12-17 10:29:02 +0100

    show this week, show this day and this months birthdays work as requiered

--- BirthdayCalendar/MainPage.xaml.cs
+++ BirthdayCalendar/MainPage.xaml.cs
@@ -1,114 +1,98 @@
-﻿//using BirthdayBackend;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using BirthdayCalendar.Models;
 using Newtonsoft.Json;
+
 namespace BirthdayCalendar
 {
-
-
     public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
-
-        public static int PersonIdToPass { get; set; }
-
-        private ObservableCollection<Person> _people;
-        public ObservableCollection<Person> People
-        {
-            get { return _people; }
-            set
-            {
-                _people = value;
-                OnPropertyChanged(nameof(People));
-            }
-        }
-        int count = 0;
+        public ObservableCollection<Person> PeopleToday { get; set; } = new ObservableCollection<Person>();
+        public ObservableCollection<Person> PeopleThisWeek { get; set; } = new ObservableCollection<Person>();
+        public ObservableCollection<Person> PeopleThisMonth { get; set; } = new ObservableCollection<Person>();
 
         public MainPage()
         {
             InitializeComponent();
-            //ShowAllRecent();
+            ShowAllRecent();
             this.BindingContext = this;
-
         }
+
         private async void ShowAllRecent()
         {
-            var httpClient = new HttpClient();
-            var url = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5279/api/Person" : "http://localhost:5279/api/Person";
-            var response = await httpClient.GetAsync(url);
-            //shows  everyone whos birhtday comes recentlly 
-            //everyone whos birthday comes up this today, week and this month
-            Debug.Write("there is a connnectios");
-
-            ThisDay();
-            ThisWeek();
-            ThisMonth();
+            await ThisDay();
+            await ThisWeek();
+            await ThisMonth();
         }
 
-        private async void ThisMonth()
+        private async Task ThisDay()
+        {
+            await LoadBirthdays((person) =>
+            {
+                var today = DateTime.Now;
+                return person.BirthDate.Day == today.Day && person.BirthDate.Month == today.Month;
+            }, PeopleToday, "Today's Birthday");
+        }
+
+        private async Task ThisWeek()
+        {
+            await LoadBirthdays((person) =>
+            {
+                var today = DateTime.Now;
+                var startOfWeek = today.AddDays(-(int)today.DayOfWeek);
+                var endOfWeek = startOfWeek.AddDays(7);
+                var personBirthday = new DateTime(today.Year, person.BirthDate.Month, person.BirthDate.Day);
+                return personBirthday >= startOfWeek && personBirthday < endOfWeek;
+            }, PeopleThisWeek, "This Week's Birthday");
+        }
+
+        private async Task ThisMonth()
+        {
+            await LoadBirthdays((person) =>
+            {
+                var currentMonth = DateTime.Now.Month;
+                return person.BirthDate.Month == currentMonth;
+            }, PeopleThisMonth, "This Month's Birthday");
+        }
+
+        private async Task LoadBirthdays(Func<Person, bool> filter, ObservableCollection<Person> targetCollection, string logMessage)
         {
             var httpClient = new HttpClient();
             var url = DeviceInfo.Platform == DevicePlatform.Android
                 ? "http://10.0.2.2:5279/api/Person"
                 : "http://localhost:5279/api/Person";
 
-            var response = await httpClient.GetAsync(url);
-
-            if (response.IsSuccessStatusCode)
+            try
             {
-                Debug.Write("there is a connnectios");
-                var content = await response.Content.ReadAsStringAsync();
-                var people = JsonConvert.DeserializeObject<IEnumerable<Person>>(content);
+                var response = await httpClient.GetAsync(url);
 
-                if (People == null)
+                if (response.IsSuccessStatusCode)
                 {
-                    People = new ObservableCollection<Person>();
+                    var content = await response.Content.ReadAsStringAsync();
+                    var people = JsonConvert.DeserializeObject<IEnumerable<Person>>(content);
+
+                    targetCollection.Clear();
+
+                    foreach (var person in people.Where(filter))
+                    {
+                        targetCollection.Add(person);
+                        Debug.WriteLine($"{logMessage}: {person.FirstName} {person.LastName}, Birthday: {person.BirthDate}");
+                    }
                 }
-
-                People.Clear();
-
-                // Get current month
-                var currentMonth = DateTime.Now.Month;
-
-                // Filter for birthdays in the current month
-                var birthdaysThisMonth = people
-                    .Where(person => person.BirthDate.Month == currentMonth);
-
-
-                // Populate the collection with filtered results
-                foreach (var person in birthdaysThisMonth)
+                else
                 {
-                    People.Add(person);
-                }
-
-                // Debug output
-                foreach (var person in People)
-                {
-                    Debug.WriteLine($"This Month's Birthday: {person.FirstName} {person.LastName}, Birthday: {person.BirthDate}");
+                    Debug.WriteLine($"Error: {response.StatusCode}");
                 }
             }
-            else
+            catch (Exception ex)
             {
-                Debug.WriteLine($"Error: {response.StatusCode}");
+                Debug.WriteLine($"Exception: {ex.Message}");
             }
         }
-
-
-        private async void ThisWeek()
-        {
-
-        }
-
-        private async void ThisDay()
-        {
-
-        }
-
 
         void OnAddPersonClicked(object sender, EventArgs e)
         {
-
             Navigation.PushAsync(new addPerson());
         }
 
